680d02bb471298d076ae1cbe4bbb050f28207fae
RemiVR <remi.vr5@gmail.com>
2015-01-19 11:30:39 +0100

    finish with first test

--- calculatortest.rb
+++ calculatortest.rb
@@ -1,34 +1,32 @@
 require 'pp'
 
 class Calculator
-	def initialize(number_1, number_2)
-		@number_1 = number_1
-		@number_2 = number_2
+	
+
+	def add(number_1, number_2)
+		number_1 + number_2 
 	end
 
-	def add
-		@number_1 + @number_2 
+	def subtract(number_1, number_2)
+		number_1 - number_2
 	end
 
-	def subtract
-		@number_1 - @number_2
+	def multiply(number_1, number_2)
+		number_1 * number_2
 	end
 
-	def multiply
-		@number_1 * @number_2
-	end
-
-	def divide
-		@number_1 / @number_2
+	def divide(number_1, number_2)
+		number_1 / number_2
 	end
 
 end
 
-calculate = Calculator.new(6,"bob")
-pp calculate.subtract
+calculate = Calculator.new
 
-# Test 1
-if "bob".to_i? 
+
+# Test 1 - = - = - = - = 
+puts "Addition test"
+if calculate.add(5,6) == 15
 	puts "OK"
 else
 	puts "FAIL"
