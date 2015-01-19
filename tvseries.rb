f040febdb555c4d01c66ffbe82d80c29556c5fbf
RemiVR <remi.vr5@gmail.com>
2015-01-19 16:03:40 +0100

    running first rspec test.

--- tvseries.rb
+++ tvseries.rb
@@ -1,32 +1,32 @@
+require 'pp'
 class SeriesMaster
 	def initialize
-		@tv_series = Hash[
+		@tv_series = {
 			"Breaking bad" => 8,
 			"Walking dead" => 7,
 			"Californication" => 9
-		]
+		}
 	end
-	def compare
-		if @tv_series["Breaking bad"] == @tv_series["Walking dead"]
-			"yay"
-		else
-			"fail"
-		end
+	def highest_rating
+		@tv_series.values.sort.last
 	end
+
+	# def compare
+	# 	if @tv_series["Breaking bad"] == @tv_series["Walking dead"]
+	# end
 end
 
-
 series_master = SeriesMaster.new
-puts series.compare
+pp series_master.highest_rating
 
 describe "SeriesMaster" do
 	before do
 		@seriesmaster = SeriesMaster.new
 	end
 
-	describe "Higher rated series"
-		it "should evaluate higher ratings" do
-			expect(true)
+	describe "Higher rated series" do
+		it "should return highest rating" do
+			expect(@seriesmaster.highest_rating)
 		end
 	end
 end
