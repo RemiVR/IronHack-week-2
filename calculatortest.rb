0add6b7a67263bac3c7ab358b7c7982fb7eb0004
RemiVR <remi.vr5@gmail.com>
2015-01-19 12:14:49 +0100

    modify file with rspec testing

--- calculatortest.rb
+++ calculatortest.rb
@@ -21,8 +21,8 @@ class Calculator
 
 end
 
+=begin
 calculate = Calculator.new
-
 
 # Test 1 - = - = - = - = 
 puts "Addition test"
@@ -30,4 +30,20 @@ if calculate.add(5,6) == 15
 	puts "OK"
 else
 	puts "FAIL"
+end
+=end
+
+describe Calculator do
+	before do
+		@calculator = Calculator.new
+	end
+	describe "#addition" do
+		it "should add 5 and 6" do
+			expect(@calculator.add(5,6)).to eq(11)
+		end
+
+		it "should add 0 and 4" do
+			expect(@calculator.add(0,4)).to eq(4)
+		end
+	end
 end
