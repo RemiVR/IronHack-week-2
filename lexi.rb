2c743ad167890f100beb8528586076d2e6ccd5dc
RemiVR <remi.vr5@gmail.com>
2015-01-20 20:12:52 +0100

    oompa loompa working on confusion

--- lexi.rb
+++ lexi.rb
@@ -1,7 +1,13 @@
 class Lexiconomitron
-	def shazam(string)
-		array = string.map {|word| word.reverse}
-		array.map {|w| w.downcase.gsub"t",""}
+	def shazam(array)
+		string = array.map {|word| word.reverse}
+		rev_string = string.map {|w| w.downcase.gsub"t",""}
+		array = [rev_string.first,rev_string.last]
+	end
+
+	def oompa_loompa(array)
+		array.select do |word| word.length <= 3 end
+
 	end
 end
 
@@ -12,7 +18,12 @@ describe "Lexiconomitron" do
 	
 	describe "shazam magic" do
 		it "should return reverse words in the array and devour letter 'T'" do
-			expect(@lexiconomitron.shazam(["This", "is", "a", "boring", "test"])).to eq(["sih", "si", "a", "gnirob", "se"])
+			expect(@lexiconomitron.shazam(["This", "is", "a", "boring", "test"])).to eq(["sih", "se"])
+		end
+	end
+	describe "confusion of oompa loompa" do
+		it "should delete words that ar longer than 3 characters" do
+			expect(@lexiconomitron.oompa_loompa(["if", "you", "wanna", "be", "my", "lover"])).to eq(["if", "you", "be", "my"])
 		end
 	end
 end
