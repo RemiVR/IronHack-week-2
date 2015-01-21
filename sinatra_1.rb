691f402e9d9916dd6c790a3c934bcabf6e76d6ba
RemiVR <remi.vr5@gmail.com>
2015-01-21 12:49:26 +0100

    uploaded

--- sinatra_1.rb
+++ sinatra_1.rb
@@ -1,20 +1,30 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
 set :port, 3000
 set :bind, '0.0.0.0'
 
+class SongList
+	def order_list
+		songs = ["Back home - Fritz Kalkbrenner", "Aaron - Paul Kalkbrenner", "facing the sun - Fritz Kalkbrenner", "Back home - Fritz Kalkbrenner",]
+		
+	end
+end
 
 get '/' do
-	songs += 1
+	list = SongList.new
+	ordered_list = list.order_list
 end
 
 post '/songs/new' do
-	songs = ["Back home - Fritz Kalkbrenner",
-		 "Aaron - Paul Kalkbrenner",
-		 "facing the sun - Fritz Kalkbrenner",
-		 "Back home -Fritz Kalkbrenner",
+	if songs >= 10
+		redirect('/enough')
+	else
+		redirect('/')
+	end
 end
 
-get '/enough' do 
 
+get '/enough' do 
+	
 end
