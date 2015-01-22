f746ae9e10453c822201ac765fb7f2dc50d41c2a
RemiVR <remi.vr5@gmail.com>
2015-01-22 09:18:36 +0100

    Good morning with a commit

--- sl9/sl9.rb
+++ sl9/sl9.rb
@@ -5,23 +5,26 @@ set :port, 3001
 set :bind, '0.0.0.0'
 
 class Songlist
-	attr_accessor :songs
 	def initialize
 		@songs = []
 	end
 	def add_song(author,name)
-		@songs = {author: author, name: name}
+		@songs << {author: author, name: name}
 	end
 end
 
 song_list = Songlist.new
 
 get '/' do
-	@song_list = song_list.songs
+
 	erb :sl9exercise
 end
 
 post '/save_song' do
-	song_list.add_song(params[:author],params[:name])
+	song_list.add_song(params[:author], params[:name])
 	redirect('/')
 end
+
+# get '/enough' do
+# 	"enough"
+# end
